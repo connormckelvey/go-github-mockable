61d9f87b071aff5159ab2a767bcb053366a2c695
Connor McKelvey <connormckelvey@gmail.com>
2022-11-13 13:31:39 -0700

    remove workfile ref

--- mocks/generate.go
+++ mocks/generate.go
@@ -1,3 +1,3 @@
 package mocks
 
-//go:generate go run -mod=mod -workfile=off github.com/golang/mock/mockgen@v1.6.0 -source=../client.gen.go -package=mocks -destination mocks.gen.go
+//go:generate go run -mod=mod github.com/golang/mock/mockgen@v1.6.0 -source=../client.gen.go -package=mocks -destination mocks.gen.go
